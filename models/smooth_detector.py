c2a57e8f4d047542d7214f93aa61fe55090bf4c2
aysamodurov <samodyrow@yandex.ru>
2022-12-01 14:58:36 +0300

    indication_list  rename

--- models/smooth_detector.py
+++ models/smooth_detector.py
@@ -14,8 +14,8 @@ class SmoothDetector(Detector):
         newKks = '{}_smooth_{}'.format(detect.get_kks(), koef)
         newDesc = '{}_with smooth_{}'.format(detect.get_description(), koef)
         super().__init__(newKks, newDesc)
-        valPr = detect.indicationList[0].value
-        for indication in detect.indicationList:
+        valPr = detect.indication_list[0].value
+        for indication in detect.indication_list:
             valPr = valPr + (indication.value-valPr)*koef
             newIndication = Indication(indication.dt, valPr, indication.status)
             self.add_indication(newIndication)
